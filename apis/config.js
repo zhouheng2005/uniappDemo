0f92af0752ddd3bf1e64c52f2ff6c272c95ed337
周珩 <7569399+zhouheng1991@user.noreply.gitee.com>
2022-04-19 12:22:23 +0800

    更新配置接口名称

--- apis/config.js
+++ apis/config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
 	//开发环境配置
 	development: {
-		assetsPath: "https://static.yfkjqh.com/", // 静态资源路径
+		assetsPath: "https://xxxxx.com/", // 静态资源路径
 		// #ifdef H5  
 		baseUrl: "/api/", //h5跨域访问设置
 		// #endif
 	},
 	//生产正式环境配置
 	production: {
-		assetsPath: "https://static.yfkjqh.com/static", // 静态资源路径
-		baseUrl: "https://api.yfkjqhw.com/api/",
+		assetsPath: "https://xxx", // 静态资源路径
+		baseUrl: "https://xxxx/api/",
 	}
 }
 export default CONFIG[process.env.NODE_ENV];
